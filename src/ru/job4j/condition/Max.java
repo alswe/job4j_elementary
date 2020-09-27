9da72d8cd3dd91095d28f718e39621629d25887a
alswe <alluginov@hotmail.com>
2020-09-27 00:06:16 -0400

    3. Перегрузить метод max для трех чисел [#285177]

--- src/ru/job4j/condition/Max.java
+++ src/ru/job4j/condition/Max.java
@@ -2,9 +2,17 @@ package ru.job4j.condition;
 
 public class Max {
 
-    public static int max(int left, int right) {
-        int result = left > right ? left : right;
-        return result;
+    public static int max(int first, int second) {
+        return first > second ? first : second;
+    }
+
+    public static int max(int first, int second, int third) {
+        int tempMax = max(first, second);
+        return third > tempMax ? third : tempMax;
+    }
+    public static int max(int first, int second, int third, int fourth) {
+        int tempMax = max(first, second, third);
+        return fourth > tempMax ? fourth : tempMax;
     }
 
 }
--- test/ru/job4j/condition/MaxTest.java
+++ test/ru/job4j/condition/MaxTest.java
@@ -24,4 +24,32 @@ public class MaxTest {
         assertThat(result, is(2));
     }
 
+    @Test
+    public void maxOfThree() {
+        assertThat(Max.max(0,0,0), is(0));
+
+        assertThat(Max.max(1,0,0), is(1));
+        assertThat(Max.max(0,1,0), is(1));
+        assertThat(Max.max(0,0,1), is(1));
+
+        assertThat(Max.max(1,2,3), is(3));
+        assertThat(Max.max(1,3,2), is(3));
+        assertThat(Max.max(3,2,1), is(3));
+    }
+
+    @Test
+    public void maxOfFour() {
+        assertThat(Max.max(0,0,0, 0), is(0));
+
+        assertThat(Max.max(1,0,0, 0), is(1));
+        assertThat(Max.max(0,1,0, 0), is(1));
+        assertThat(Max.max(0,0,1, 0), is(1));
+        assertThat(Max.max(0,0,0, 1), is(1));
+
+        assertThat(Max.max(1,2,3, 4), is(4));
+        assertThat(Max.max(1,2,4, 3), is(4));
+        assertThat(Max.max(1,4,2, 3), is(4));
+        assertThat(Max.max(4,1,2, 3), is(4));
+    }
+
 }
